8a2c2090f350a6412ffd6bce31a73ed657568af2
Sapfires <nastya-zykov@tut.by>
2021-04-25 22:43:18 +0300

    SubTask10

---
+++ src/main/java/com/epam/intro/SubTask10.java
@@ -0,0 +1,25 @@
+package com.epam.intro;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SubTask10 {
+    public static void main(String[] args) {
+        String str = readStringVariable("string: ");
+        Pattern pattern = Pattern.compile("[a]{1}");
+        Matcher matcher = pattern.matcher(str);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        System.out.println(count);
+    }
+
+
+    public static String readStringVariable(String variableName) {
+        System.out.println("Please input " + variableName);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
