7fb18cdf26f0f785ae6acb099155c785d127f4e2
Sapfires <nastya-zykov@tut.by>
2021-04-25 22:41:20 +0300

    SubTask9

---
+++ src/main/java/com/epam/intro/SubTask9.java
@@ -0,0 +1,8 @@
+package com.epam.intro;
+
+public class SubTask9 {
+    public static void main(String[] args) {
+        String originalString = "информатика";
+        System.out.println(originalString.substring(7,8) + originalString.substring(3,5) + originalString.substring(7,8));
+    }
+}
